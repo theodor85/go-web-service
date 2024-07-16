e38afe96d5fa14e5cb0044f629fa75009d01ce03
Teddy Coder <fedor_coder@mail.ru>
2024-07-16 16:35:10 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync/atomic"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	var counter atomic.Uint64
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/counter", func(w http.ResponseWriter, r *http.Request) {
+		answer := fmt.Sprintf("The counter is %d", counter.Load())
+		w.Write([]byte(answer))
+	})
+
+	r.Post("/counter", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		str_new_value := r.FormValue("value")
+
+		new_value, err := strconv.Atoi(str_new_value)
+		if err != nil {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			w.Write([]byte("Invalid value"))
+			return
+		}
+
+		counter.Store(uint64(new_value))
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("Counter saved"))
+	})
+
+	http.ListenAndServe(":3000", r)
+}
+
